8827192598c6d9cccc44981ba8549bf0eef30ab4
Sercan <sercansimsek44@gmail.com>
2025-08-26 12:24:40 +0200

    test

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getData } from "../../data/api";
 import { Card } from "../Card/Card";
+import mainImage from "../../assets/images/hero-image-sm.jpg";
 
 export const CardList = () => {
 	const [properties, setProperties] = useState([]);
@@ -23,6 +24,16 @@ export const CardList = () => {
 
 	return (
 		<div>
+			<div className="relative ">
+				<img className="max-w-[412px] h-[511px] object-fill object-center" src={mainImage} alt="mainHeroImage" />
+				<div className="flex flex-col justify-start absolute inset-18 text-[#121826]">
+					<h1 className="font-bold text-[40px] ">
+						Peace, nature, <br /> dream
+					</h1>
+
+					<p className="font-medium text-2xl">Find and book a great experience</p>
+				</div>
+			</div>
 			<ul className="flex flex-col gap-6">
 				{properties.map((property) => (
 					<li key={property.id}>{<Card property={property} />}</li>
